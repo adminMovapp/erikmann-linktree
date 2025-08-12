eac735102585724b89aad73e5030a4cc7fb08272
adminMovapp <admin@movapp.org>
2025-08-12 16:16:14 -0600

    feat: Estructura inicial de proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,36 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+   integrations: [tailwind(), react()],
+
+   vite: {
+      // plugins: [tailwindcss()],
+      resolve: {
+         alias: {
+            '@': '/src/',
+            '@components': '/src/components/',
+            '@layouts': '/src/layouts/',
+            '@pages': '/src/pages/',
+            '@styles': '/src/styles/',
+            '@assets': '/src/assets/',
+            '@hooks': '/src/hooks/',
+            '@utils': '/src/utils/',
+            '@api': '/src/api/',
+            '@constants': '/src/constants/',
+         },
+      },
+      // assetsInclude: ['**/*.json']
+   },
+
+   server: {
+      host: true,
+      port: 7002,
+      headers: {
+         'X-Frame-Options': 'SAMEORIGIN',
+         'X-Content-Type-Options': 'nosniff',
+         'Referrer-Policy': 'strict-origin-when-cross-origin',
+      },
+   },
+});
---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+   theme: {
+      extend: {},
+   },
+   content: [
+      './src/**/*.{astro,html,js,jsx,ts,tsx}',
+      './components/**/*.{astro,js,jsx,ts,tsx}',
+      './layouts/**/*.{astro,js,jsx,ts,tsx}',
+   ],
+   plugins: [],
+};
